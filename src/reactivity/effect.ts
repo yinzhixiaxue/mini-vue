f85eb0dd0e0bc2e130c404601f5c4b606a6ea593
yinzhixiaxue <775288786@qq.com>
2022-08-06 16:59:59 +0800

    重新实现effect和reactive的happy path

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,23 +1,28 @@
-class Reactiveffect {
-  private _fn;
-  constructor(fn, public scheduler?) {
-    this._fn = fn
-    this.scheduler = scheduler
+const _fn = Symbol('_fn');
+class ReactiveEffect {
+  private _fn: any
+  constructor(fn) {
+    this[_fn] = fn
   }
   run() {
     activeEffect = this
-    return this._fn()
+    this[_fn]()
   }
 }
 
+let activeEffect
+export function effect(fn) {
+  let effect = new ReactiveEffect(fn)
+  effect.run()
+}
+
+// target -> key -> dep
 let targetMap = new Map()
-let activeEffect;
 export function track(target, key) {
-  // target => dep => key
-  // 把target都当成key存到map里面，值是depsMap，然后再把dep当成key存进map，值是实例对象
   let depsMap = targetMap.get(target)
   if (!depsMap) {
     depsMap = new Map()
+    // 别忘了set。。。
     targetMap.set(target, depsMap)
   }
   let dep = depsMap.get(key)
@@ -25,24 +30,14 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  // 此处要加的是实例，而不是fn
   dep.add(activeEffect)
 }
 
 export function trigger(target, key) {
   let depsMap = targetMap.get(target)
-  let deps = depsMap.get(key)
-  // 把当前实例的所有需要触发的方法，全都执行一遍
-  for (let item of deps) {
-    if (item.scheduler) {
-      item.scheduler()
-    } else {
-      item.run()
-    }
+  let dep = depsMap.get(key)
+  for (let effect of dep) {
+    effect.run()
   }
-}
-export function effect(fn, options: any = {}) {
-  const { scheduler } = options
-  const _effect = new Reactiveffect(fn, scheduler)
-  _effect.run()
-  return _effect.run.bind(_effect)
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,15 +1,19 @@
-import { track, trigger } from './effect'
-export function reactive(raw): any {
+import {
+  track, trigger
+} from './effect'
+export function reactive(raw) {
   return new Proxy(raw, {
-    get(target, key) {
+    get: function (target, key) {
       const res = Reflect.get(target, key)
       track(target, key)
       return res
+      // return Reflect.get(target, key)
     },
-    set(target, key, value) {
+    set: function (target, key, value) {
       const res = Reflect.set(target, key, value)
       trigger(target, key)
       return res
+      // return Reflect.set(target, key, value)
     }
   })
 }
